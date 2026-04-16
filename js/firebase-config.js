23b29e5c1b07e07368cc07dac4e056b6c9f374eb
DanL <bengbcit@gmail.com>
2026-04-16 15:00:54 +0900

    update firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,13 +9,27 @@
    6. Paste your values below
 */
 
-const FIREBASE_CONFIG = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyDFVkbF9NHn9F8gv2_JS6uRbwE38VDY4CY",
   authDomain: "study-reminder-8910.firebaseapp.com",
   projectId: "study-reminder-8910",
   storageBucket: "study-reminder-8910.firebasestorage.app",
   messagingSenderId: "122788831469",
   appId: "1:122788831469:web:fd6f652f9ab5d90c39ace4",
+  measurementId: "G-M178C4EY4Q"
 };
 
-export default FIREBASE_CONFIG;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// 导出 app 实例，以便其他文件使用
+export default app;
